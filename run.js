16c9ae4e2e95d6d296184b8a78aaa500e5a2c98f
SyntaxStacks <johnbobsexipants@gmail.com>
2025-07-29 01:56:50 -0700

    deploy to render

--- run.js
+++ run.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 const { scanMarket } = require('./src/scanner');
 const { scanCrypto } = require('./src/cryptoScanner');
 
+const timeout = 1000 * 60 * 60;
+
 (async () => {
-  await scanMarket();
-  await scanCrypto();
+  while (true) {
+    console.log("🔁 Running scanner...");
+    await scanMarket();
+    await scanCrypto();
+    console.log("⏳ Waiting 1 hour...");
+    await new Promise(res => setTimeout(res, timeout)); // 1 hour
+  }
 })();
+
